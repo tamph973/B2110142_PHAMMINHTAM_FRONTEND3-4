e4e2e11260314ba7bb40bb4dc181f39c0fae73ad
tamph973 <phtam0907@gmail.com>
2023-10-05 01:47:06 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="page">
+        <p>
+            Oops, không thể tìm thấy trang. Trở về.
+            <router-link to="/">Về Trang chủ.</router-link>
+        </p>
+    </div>
+</template>
