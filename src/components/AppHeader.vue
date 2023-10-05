4ed709ff248c0ec31462be525c66e0b6fa170e46
tamph973 <phtam0907@gmail.com>
2023-10-05 19:49:41 +0700

    Tao trang cap nhat lien he

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,10 +5,11 @@
         <div class="mr-auto navbar-nav">
             <li class="nav-item">
                 <router-link :to="{name:'contactbook'}" class="nav-link">
-                Danh bạ
-                <i class="fas fa-address-book"></i>
-            </router-link>
+                    Danh bạ
+                    <i class="fas fa-address-book"></i>
+                </router-link>
             </li>
         </div>
+
     </nav>
 </template>
